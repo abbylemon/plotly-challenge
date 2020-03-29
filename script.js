832ca932b59560560251243277796580af70384a
physics_nerd <abby.lemon22@gmail.com>
2020-03-28 23:23:05 -0500

    fixed bubble plot to adjust marker size based on data values

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
 
 function init() {
     d3.json("./samples.json").then(function(data) {
-        // console.log(data);
-        // populate the dropdown section with all of the name values
+        // grab the data that we want to populate the dropdown menu with
         var names = data.names;
-
         var dropdownMenu = d3.select("#selDataset");
-
         names.forEach(function(name) {
             dropdownMenu.append("option")
                 .attr("value", name)
@@ -56,67 +53,47 @@ function DemoInfo(id) {
 // function to update bar plot and bubble plot
 function BuildCharts(id) {
     d3.json("./samples.json").then(function(data) {
+        // grab the data that we need and filter it based on the id
         var samples = data.samples;
-        // samples.forEach(function(sample) {
-        //     sample.id = String(sample.id);
-        // });
         var filterResult = samples.filter(x => x.id == id)[0];
-        var otu_ids = filterResult.otu_ids.slice(0,10).reverse();
-        var sample_values = filterResult.sample_values.slice(0,10).reverse();
-        var otu_labels = filterResult.otu_labels.slice(0,10).reverse();
-        // var otu_ids = filterResult.otu_ids.slice(0,10).reverse();
-        // var sample_values = filterResult.sample_values.slice(0,10).reverse();
-        // var otu_labels = filterResult.otu_labels.slice(0,10).reverse();
+        var otu_ids = filterResult.otu_ids;
+        var sample_values = filterResult.sample_values;
+        var otu_labels = filterResult.otu_labels;
 
-        // var sortedData = data.sort((a,b) =>
-        //     b.sample_values - a.sample_values);
-        // var slicedData = sortedData.slice(0,10);
-        // var reversedData = slicedData.reverse();
-
+        // grab the first 10 values since they are already sorted
+        var otu_ids_10 = filterResult.otu_ids.slice(0,10).reverse();
+        var sample_values_10 = filterResult.sample_values.slice(0,10).reverse();
+        var otu_labels_10 = filterResult.otu_labels.slice(0,10).reverse();
+        
+        // create the plot for the horizonal bar pot
         var trace = {
-            // x: sample_values.slice(0,10).reverse(),
-            // y: otu_ids.slice(0,10).reverse(),
-            // text: otu_labels.slice(0,10).reverse(),
-            x: sample_values,
-            y: otu_ids.map(d=>"OTU " + d),
-            text: otu_labels,
+            x: sample_values_10,
+            y: otu_ids_10.map(d=>"OTU " + d),
+            text: otu_labels_10,
             type: "bar",
             orientation: "h"
         };
     
         var barData = [trace];
-    
-        var layout = {
-            title: (`Top 10 OTU's for Sample ${id}`),
-            // xaxis: {title: "Sample Value"},
-            // yaxis: {title: "OTU Type"}
-        };
 
-        Plotly.newPlot("bar", barData, layout);
+        Plotly.newPlot("bar", barData);
 
+        // create the bubble plot
         var traceB = {
             x: otu_ids,
             y: sample_values,
+            text: otu_labels,
             type: "scatter",
             mode: "markers",
-            markers: {
+            marker: {
                 color: otu_ids,
-                size: sample_values,
-                colorscale: "Earth"
+                size: sample_values
             }
         }
 
         var bubbleData = [traceB];
 
-        var layoutB = {
-            title: "Bubble Plot",
-            margin: {
-                t: 30,
-                l: 50
-            }
-        }
-
-        Plotly.newPlot("bubble", bubbleData, layoutB);
+        Plotly.newPlot("bubble", bubbleData);
 
     });
     
